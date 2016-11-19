b80697d825191caa54aa472779e57775dc05e606
gittina <martina.paganin95@gmail.com>
2016-11-19 22:41:49 +0100

    ok functions for parameters

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ function visualizza(){
     tab2.style.visibility="visible"
     
     var button_prod = document.getElementById("button_products");
-    button_prod.style.visibility="visible"
+    button_prod.style.visibility="visible"    
+}
+
+
+function inserisci(){
+    var cA = Number(document.getElementById("p1").value);
+    var cB = Number(document.getElementById("p2").value);
+    
+    var prodA = document.getElementById("A").value;
+    
+    var ris = cA+cB;
+    
+    console.log(typeof(prodA));
+    
+    var prodX = document.getElementById("prodX").value;
+    var qX = document.getElementById("qX").value;
+    
+    document.getElementById("p1").value = ris;
+    
+    console.log(prodX);
+    console.log(qX);
     
     
     
