276f8e02bcfd2473a4cd981ad8fdfccf2de8a7f7
gittina <martina.paganin95@gmail.com>
2016-11-19 14:33:24 +0100

    funziona visibilità tabella

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function visualizza(){
-    var el = document.getElementById("tab2");
-    el.style.visibility="visible"
+    var tab2 = document.getElementById("tab2");
+    tab2.style.visibility="visible"
+    
+    var button_prod = document.getElementById("button_products");
+    button_prod.style.visibility="visible"
+    
+    
     
     
 }
